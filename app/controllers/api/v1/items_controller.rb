e96eb70b699787aa894ce58f7b22f6a97dcda201
Sully Birashk <sully.birashk@gmail.com>
2022-03-29 10:51:59 -0700

    Add Items Controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,33 @@
 class Api::V1::ItemsController < ApplicationController
 
   def index
-    render json: Item.where(merchant_id: params[:merchant_id])
+    render json: Item.all
   end
+
+  def show
+    render json: Item.find(params[:id])
+  end
+
+  def create
+    render json: Item.create!(item_params)
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    render json: item
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    render json: item
+  end
+
+  private
+
+  def item_params
+    params.permit(:name, :description, :unit_price, :merchant_id)
+  end
+
 end
