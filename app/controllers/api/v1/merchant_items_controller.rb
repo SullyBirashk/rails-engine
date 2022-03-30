b49defe273279f7e652f2ad9e6fa0971e5532c3e
Sully Birashk <sully.birashk@gmail.com>
2022-03-30 10:28:32 -0700

    Merchant Items Controller

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MerchantItemsController < ApplicationController
 
   def index
-    render json: MerchantItemSerializer.new(Item.where(merchant_id: params[:merchant_id]))
+    render json: ItemSerializer.new(Item.where(merchant_id: params[:merchant_id]))
   end
 end
