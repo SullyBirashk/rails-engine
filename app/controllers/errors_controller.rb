0f8f00537414d96daa126e9ebd33ea44ab8797c1
Sully Birashk <sully.birashk@gmail.com>
2022-03-31 18:49:05 -0600

    Delete unessesarry content

--- app/controllers/errors_controller.rb
+++
@@ -1,9 +0,0 @@
-class ErrorsController < ActionController
-  def not_found
-    if get "/api/v1/merchants/:merchant_id/items" =~ /^\/api/
-      render :json => {:status => "not-found"}.to_json, :code => 404
-    else
-      render :text => "404 Not found", :status => 404 # You can render your own template here
-    end
-  end
-end
